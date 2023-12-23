0a2a0aba4e8c26c070ae151c26925b96672bd1b7
ianirudhkhabya <ianirudhkhabya@gmail.com>
2023-12-23 13:46:54 +0530

    ninth commit

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,26 @@
-export default function Customers() {
-  return <h1>Customers page</h1>;
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+type PageProps = {
+  searchParams?: {
+    query?: string;
+  };
+};
+
+export default async function Page({ searchParams }: PageProps) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+      <Table customers={customers} />
+    </Suspense>
+  );
 }
